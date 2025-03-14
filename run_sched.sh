809b8df1b746ed7fc407b9e38f245a9d0ef2b0e1
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2025-03-14 16:27:20 +0700

    update run script

--- run_sched.sh
+++ run_sched.sh
@@ -27,3 +27,45 @@ echo "Running os_1_singleCPU_mlq_paging"
 python3 ganttchart.py m_output os_1_singleCPU_mlq_paging.output
 python3 ganttchart.py output os_1_singleCPU_mlq_paging.output
 echo "Done"
+
+echo "Running os_0_mlq_paging"
+./os os_0_mlq_paging > m_output/os_0_mlq_paging.output
+python3 ganttchart.py m_output os_0_mlq_paging.output
+python3 ganttchart.py output os_0_mlq_paging.output
+echo "Done"
+
+echo "Running os_1_mlq_paging"
+./os os_1_mlq_paging > m_output/os_1_mlq_paging.output
+python3 ganttchart.py m_output os_1_mlq_paging.output
+python3 ganttchart.py output os_1_mlq_paging.output
+echo "Done"
+
+echo "Running os_1_mlq_paging_small_1K"
+./os os_1_mlq_paging_small_1K > m_output/os_1_mlq_paging_small_1K.output
+python3 ganttchart.py m_output os_1_mlq_paging_small_1K.output
+python3 ganttchart.py output os_1_mlq_paging_small_1K.output
+echo "Done"
+
+echo "Running os_1_mlq_paging_small_4K"
+./os os_1_mlq_paging_small_4K > m_output/os_1_mlq_paging_small_4K.output
+python3 ganttchart.py m_output os_1_mlq_paging_small_4K.output
+python3 ganttchart.py output os_1_mlq_paging_small_4K.output
+echo "Done"
+
+echo "Running os_sc"
+./os os_sc > m_output/os_sc.output
+python3 ganttchart.py m_output os_sc.output
+python3 ganttchart.py output os_sc.output
+echo "Done"
+
+echo "Running os_syscall"
+./os os_syscall > m_output/os_syscall.output
+python3 ganttchart.py m_output os_syscall.output
+python3 ganttchart.py output os_syscall.output
+echo "Done"
+
+echo "Running os_syscall_list"
+./os os_syscall_list > m_output/os_syscall_list.output
+python3 ganttchart.py m_output os_syscall_list.output
+python3 ganttchart.py output os_syscall_list.output
+echo "Done"
