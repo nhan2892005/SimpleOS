34a0bb92fce920cda802869e8080914dd7a931b6
DQLong <long.doquang1896@hcmut.edu.vn>
2025-04-05 15:41:43 +0700

    Update queue.c

--- src/queue.c
+++ src/queue.c
@@ -9,12 +9,30 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
         /* TODO: put a new process to queue [q] */
+	if(q->size == MAX_QUEUE_SIZE){
+                return;
+        }
+        q->proc[q->size] = proc; 
+        q-> size++;
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-	return NULL;
+	if (q->size == 0)       
+                return NULL;
+ 
+        struct pcb_t* proc = NULL;
+
+        proc = q->proc[0];
+
+        for(int i = 1 ; i < q->size ; i++){
+                q->proc[i - 1] = q->proc[i];
+        }
+
+        q-> size--;
+
+        return proc;
 }
 
